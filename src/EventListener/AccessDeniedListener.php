ecbe31f2831615790d53307f79e5b2e1016d2c0c
Notizou <noah.decroix3@gmail.com>
2023-10-31 16:13:34 +0100

    Add tailwind + voter and event for access denied and other kernel exception

--- src/EventListener/AccessDeniedListener.php
+++ src/EventListener/AccessDeniedListener.php
@@ -1,8 +1,46 @@
 <?php
 
+
 namespace App\EventListener;
 
-class AccessDeniedListener
-{
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
+class AccessDeniedListener implements EventSubscriberInterface
+{
+    private UrlGeneratorInterface $urlGenerator;
+
+    public function __construct(UrlGeneratorInterface $urlGenerator)
+    {
+        $this->urlGenerator = $urlGenerator;
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            // the priority must be greater than the Security HTTP
+            // ExceptionListener, to make sure it's called before
+            // the default exception listener
+            KernelEvents::EXCEPTION => ['onKernelException', 2],
+        ];
+    }
+
+    public function onKernelException(ExceptionEvent $event): void
+    {
+        $exception = $event->getThrowable();
+        $redirectToRoute = $this->urlGenerator->generate('app_home');
+        $response = new RedirectResponse($redirectToRoute);
+
+        if ($exception instanceof AccessDeniedException) {
+
+            $redirectToRoute = $this->urlGenerator->generate('app_home');
+            $response = new RedirectResponse($redirectToRoute);
+        }
+        $event->setResponse($response);
+    }
 }
--- src/Security/VideoVoter.php
+++ src/Security/VideoVoter.php
@@ -2,7 +2,47 @@
 
 namespace App\Security;
 
-class VideoVoter
-{
+use App\Entity\Video;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+use Symfony\Component\Security\Core\User\UserInterface;
 
+class VideoVoter  extends Voter
+{
+    public const EDIT = "VIDEO_EDIT";
+    public const DELETE = "VIDEO_DELETE";
+
+    private Security $security;
+
+    /**
+     * @param Security $security
+     */
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+
+
+    protected function supports(string $attribute, mixed $subject): bool
+    {
+        return in_array($attribute, [self::EDIT, self::DELETE])
+            && $subject instanceof Video;
+    }
+
+    protected function voteOnAttribute(string $attribute, mixed $subject, TokenInterface $token): bool
+    {
+        $user = $token->getUser();
+        if (!$user instanceof UserInterface) {
+            return false;
+        }
+
+        switch ($attribute) {
+            case self::DELETE:
+            case self::EDIT:
+                return $this->security->isGranted("ROLE_ADMIN");
+
+        }
+        return false;
+    }
 }
